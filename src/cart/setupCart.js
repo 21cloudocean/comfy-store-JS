382e9647784a1b523bb11e2ad5f144a5ea0c44bf
21cloudocean <437508630@qq.com>
2022-07-22 22:46:23 +0800

    JS displayCartItemCount+displayCartTotal

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -9,7 +9,7 @@ import { openCart } from "./toggleCart.js";
 import { findProduct } from "../store.js";
 import addToCartDOM from "./addToCartDOM.js";
 // set items
-const catItemCountDOM = getElement(".cart-item-count");
+const cartItemCountDOM = getElement(".cart-item-count");
 const cartItemsDOM = getElement(".cart-items");
 const cartTotalDOM = getElement(".cart-total");
 
@@ -27,11 +27,28 @@ export const addToCart = (id) => {
   } else {
     // update values
   }
-
+  // add one to the item count
+  displayCartItemCount();
+  // display cart totals
+  displayCartTotal();
+  // set cart in local storage
+  setStorageItem("cart", cart);
   // more stuff coming up
   openCart();
 };
+function displayCartItemCount() {
+  const amount = cart.reduce((total, cartItem) => {
+    return (total += cartItem.amount);
+  }, 0);
+  cartItemCountDOM.textContent = amount;
+}
 
+function displayCartTotal() {
+  let total = cart.reduce((total, cartItem) => {
+    return (total += cartItem.price * cartItem.amount);
+  }, 0);
+  cartTotalDOM.textContent = `Total:${formatPrice(total)}`;
+}
 const init = () => {
   console.log(cart);
 };
